6418e5e2b0620b435b48c3d62a0a3f5b56992a4a
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2026-09-19 17:14:33 +0700

    исправил замечания ревьюера

--- database/migrations/0001_create_users.up.sql
+++ database/migrations/0001_create_users.up.sql
@@ -12,29 +12,3 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE IF NOT EXISTS curriculum_vitae (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT UNIQUE NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    cv_file_id TEXT NOT NULL,
-    cv_path TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TYPE grade AS ENUM ('INTERN', 'JUNIOR', 'MIDDLE', 'SENIOR', 'LEAD');
-
-CREATE TYPE employment_type AS ENUM ('REMOTE', 'OFFICE', 'HYBRID', 'CONTRACT');
-
--- User search settings table
-CREATE TABLE IF NOT EXISTS user_settings (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT UNIQUE NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    skills jsonb DEFAULT '[]'::jsonb,
-    grade grade,  -- Intern/Junior/Senior and etc
-    employment_type employment_type,  -- Such as remote, contract, etc.
-    location TEXT,  -- If specified - country/city
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE INDEX IF NOT EXISTS idx_users_username ON users(username) WHERE username IS NOT NULL;
